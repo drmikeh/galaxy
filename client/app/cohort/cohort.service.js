cb4f6d8f0a94f881fae34beee1a0f27c1b871aaa
Mike Hopper <drmikeh@gmail.com>
2015-12-01 16:36:12 -0500

    Minor tweaks.

--- client/app/cohort/cohort.service.js
+++ client/app/cohort/cohort.service.js
@@ -1,18 +1,14 @@
 'use strict';
 
 angular.module('galaxyApp')
-  .service('cohortService', function () {
+  .service('cohortService', function() {
     this.cohort = {
       name: 'ATL WDI #5',
       students: [
         { name: 'Clark Kent', age: 27 },
         { name: 'Bruce Wayne', age: 33 },
         { name: 'Oliver Queen', age: 24 },
-        { name: 'Barbara Gordon', age: 22 },
-        { name: 'Barry Allen', age: 21 },
-        { name: 'Tony Stark', age: 21 },
-        { name: 'Elon Musk', age: 39 },
-        { name: 'Steve Jobs', age: 44 }
+        { name: 'Barry Allen', age: 21 }
       ]
     };
   });
